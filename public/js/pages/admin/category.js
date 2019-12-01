3ffb38224b99762ef1a1991d8d228ea61afb89f5
Andrew Wijaya <jun.quan.mail@gmail.com>
2019-12-02 01:09:09 +0700

    add pagination functionality

--- public/js/pages/admin/category.js
+++ public/js/pages/admin/category.js
@@ -1,5 +1,7 @@
 document.title = 'Categories'
 $('#add-error-message').hide()
+let size = 10
+let page = 0
 validateAdminToken(getCookie('access-token'), (err) => window.location.href = '/admin-login')
 
 function toDataUrl(url, callback) {
@@ -149,15 +151,52 @@ async function submitButton() {
 
 }
 
-$(document).ready(async function () {
+$("#show-category").change(function() {
+    size = $(this).val()
+    getCategoriesData()
+})
+
+function changePage(index) {
+    page = parseInt(index)
+    getCategoriesData()
+}
+
+async function getCategoriesData() {
     try {
-        const response = await api.get('/categories', {
+        const response = await api.get(`/categories/paginate?size=${size}&page=${page}`, {
             headers: {
                 "Authorization": "Bearer " + getCookie("access-token")
             }
         })
+        
+        let pagination = $("#pagination")
+        let totalPages = response.data.total_pages
+        let totalElements = response.data.total_elements
+        pagination.empty()
+        pagination.append(`
+            <li class="page-item ${page == 0 ? 'disabled' : ''}">
+                <a class="page-link" onclick="changePage('${page-1}')" aria-label="Previous">
+                    <span aria-hidden="true">«</span>
+                </a>
+            </li>
+        `)
+        for (let i = 0; i < totalPages; i++) {
+            pagination.append(`
+                <li class="page-item ${i == page ? 'active' : ''}">
+                    <a class="page-link" onclick="changePage('${i}')">${i+1}</a>
+                </li>
+            `)
+        }
+        pagination.append(`
+            <li class="page-item ${page >= totalPages-1 ? 'disabled' : ''}">
+                <a class="page-link" onclick="changePage('${page+1}')" aria-label="Next">
+                    <span aria-hidden="true">»</span>
+                </a>
+            </li>
+        `)
 
         let table = $("#dataTable tbody")
+        table.empty()
         response.data.data.forEach(function(elem) {
             table.append(`
             <tr>
@@ -174,6 +213,12 @@ $(document).ready(async function () {
                 </td>
             </tr>`)
         })
+
+        $('#dataTable_info').text(`Showing ${size*page + 1} to ${size * (page + 1) > totalElements ? totalElements : size * (page + 1)} of ${totalElements}`)
     } catch (err) {
     }
+}
+
+$(document).ready(async function () {
+    getCategoriesData()
 })
