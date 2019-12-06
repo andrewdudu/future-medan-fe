f19696e7ff91d682b9916caae95dd64ec9a51f7b
syntialai <53588149+syntialai@users.noreply.github.com>
2019-12-06 21:33:57 +0700

    Fix error message in login page

--- public/js/pages/login.js
+++ public/js/pages/login.js
@@ -1,6 +1,7 @@
-$("#login-error-message").hide();
-$("#signup-error-message").hide();
 document.title = 'Login'
+let inputWrong = "Make sure your username or email and password correct."
+let somethingWrong = "Something went wrong, please try again."
+let passwordWrong = "Password does not match."
 
 $('#login-button').click(async e => {
     e.preventDefault();
@@ -17,8 +18,10 @@ $('#login-button').click(async e => {
         appState = response.data.accessToken;
 
         window.location.href = "/";
-    } catch (err) {
-        $("#login-error-message").show();
+    }
+    catch (err) {
+        addErrorMsg("login", inputWrong);
+        $("#login-error").show();
     }
 })
 
@@ -33,21 +36,15 @@ $("#register-button").click(async e => {
     let radio = $("input[type='radio']:checked").val();
 
     if (password !== confirmPassword) {
-        $("#signup-error-message").append(`
-            <div id="signup-error-message" class="alert alert-danger alert-dismissible fade show">
-                <strong>Error!</strong> 
-                <span id="signup-error-message-span">Password does not match.</span>
-                <button type="button" class="close" data-dismiss="alert">&times;</button>
-            </div>`)
-        $("#signup-error-message").show();
-    } else {
-
+        addErrorMsg("signup", passwordWrong)
+        $("#signup-error").show();
+    }
+    else {
         try {
             let path = "/user/register";
     
             if (radio === "merchant") path = "/merchant/register"; 
             
-    
             const response = await api.post(path, {
                 username,
                 email,
@@ -57,50 +54,33 @@ $("#register-button").click(async e => {
     
             console.log(response)
             window.location.href = "/login";
-        } catch (err) {
-            console.log(err.response);
+        }
+        catch (err) {
+            console.log(err.response)
+
             if (err.response === undefined) {
-                $("#signup-error-message").append(`
-                <div id="signup-error-message" class="alert alert-danger alert-dismissible fade show">
-                    <strong>Error!</strong> 
-                    <span id="signup-error-message-span">Something went wrong, please try again.</span>
-                    <button type="button" class="close" data-dismiss="alert">&times;</button>
-                </div>`)
-            } else {
+                addErrorMsg("signup", somethingWrong)
+            }
+            else {
                 let errors = err.response.data.success ? err.response.data.message : err.response.data.errors;
                 let message = "";
         
-                $("#signup-error-message").empty();
+                $("#signup-error").empty();
                 if (Array.isArray(errors)) {
                     errors.forEach(errMessage => {
-                        let el = `
-                        <div id="signup-error-message" class="alert alert-danger alert-dismissible fade show">
-                            <strong>Error!</strong> 
-                            <span id="signup-error-message-span">${errMessage.field} ${errMessage.defaultMessage}</span>
-                            <button type="button" class="close" data-dismiss="alert">&times;</button>
-                        </div>`
-                        $("#signup-error-message").append(el);
+                        $("#signup-error").append(errorHTML("signup", `${errMessage.field} ${errMessage.defaultMessage}`));
                     })
-                } else if (err.response.data.message) {
-                    $("#signup-error-message").append(`
-                    <div id="signup-error-message" class="alert alert-danger alert-dismissible fade show">
-                        <strong>Error!</strong> 
-                        <span id="signup-error-message-span">${err.response.data.message}</span>
-                        <button type="button" class="close" data-dismiss="alert">&times;</button>
-                    </div>`)
-                } else {
-                    $("#signup-error-message").append(`
-                    <div id="signup-error-message" class="alert alert-danger alert-dismissible fade show">
-                        <strong>Error!</strong> 
-                        <span id="signup-error-message-span">Something went wrong, please try again.</span>
-                        <button type="button" class="close" data-dismiss="alert">&times;</button>
-                    </div>`)
+                }
+                else if (err.response.data.message) {
+                    $("#signup-error").append(errorHTML("signup", err.response.data.message))
+                }
+                else {
+                    $("#signup-error").append(errorHTML("signup", somethingWrong))
                 }
             }
             
-            $("#signup-error-message").show();
+            $("#signup-error").show();
         }
-
     }
 })
 
@@ -112,3 +92,16 @@ $("#show-password").click(e => {
         passwordInput.attr('type', 'password');
     }
 })
+
+function addErrorMsg(event, msg) {
+    $(`#${event}-error`).empty()
+    $(`#${event}-error`).append(errorHTML(event, msg))
+}
+
+function errorHTML(event, msg) {
+    return `<div id="${event}-error-message" class="alert alert-danger alert-dismissible fade show">
+                <strong>Error!</strong>
+                <span id="${event}-error-message-span">${msg}</span>
+                <button type="button" class="close" data-dismiss="alert">&times;</button>
+            </div>`
+}
