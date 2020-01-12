9c073f1136cb20bb9080110b7ebf82093c49b833
Andrew Wijaya <jun.quan.mail@gmail.com>
2020-01-12 14:25:44 +0700

    integrate product page

---
+++ public/js/pages/product-by-categories.js
@@ -0,0 +1,43 @@
+document.title = "Products"
+
+$(document).ready(async e => {
+    try {
+        let categoryId = get('category')
+        let categoryName = get('name')
+
+        let response
+
+        if (categoryId !== undefined) {
+            response = await api.get(`/products/category/${categoryId}`)
+            $('#title').text(name ? name : '')
+        }
+        else {
+            response = await api.get(`/products`)
+            $('#title').text('All Products')
+        }
+        const data = response.data.data
+
+        if (!$.isArray(data) ||  !data.length) {
+            addEmptyPict("all-categories", "assets/img/illustration/empty.png")
+            console.log("empty")
+        } else {
+            const html = generateProductHTML(data)
+            $("#all-categories").removeClass("bg-white")
+            $("#products-by-category").html(html)
+            console.log("fill")
+        }
+    }
+    catch (err){
+    }
+})
+
+function generateProductHTML(products) {
+    return products.map(product => {
+        return `
+            <a id="product-link" href="#" style="background-color: white!important;">
+                <div id="product-home"><img id="product-image-home" src="${APP_URL}${product.image}">
+                    <div id="product-details-home"><span id="product-name-home">${product.name}</span><span id="product-price-home">Rp ${new Intl.NumberFormat('ID').format(product.price)}</span></div>
+                </div>
+            </a>`
+    })
+}
